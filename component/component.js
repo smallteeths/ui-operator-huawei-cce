6221b0a9891ea5ccecaae6a05805f851a1c564b3
huacheng <cheng@HuachengdeMacBook-Pro.local>
2022-08-16 16:05:39 +0800

    fix(system): Fix system typs by containerNetworkMode

--- component/component.js
+++ component/component.js
@@ -602,10 +602,9 @@ export default Ember.Component.extend(ClusterDriver, {
 
   nodeOperationSystemContent: computed('config.masterVersion', function() {
     const types = ['EulerOS 2.9', 'CentOS 7.6', 'EulerOS 2.5'];
+    const containerNetworkMode = get(this, 'config.containerNetworkMode');
 
-    const masterVersion = get(this, 'config.masterVersion');
-
-    if(masterVersion !== 'v1.19'){
+    if(containerNetworkMode !== 'overlay_l2'){
       types.push('Ubuntu 18.04');
     }
 
